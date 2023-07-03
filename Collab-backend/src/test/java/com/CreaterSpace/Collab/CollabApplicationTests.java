5f3e79ad51765085f8ab231ff0ceaa99ff33de6d
Prabin Neupane <neupaneprabin693@gmail.com>
2023-07-03 13:24:53 +0545

    All  required dependency added
    All initialization is completed for backend.

---
+++ Collab-backend/src/test/java/com/CreaterSpace/Collab/CollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CreaterSpace.Collab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
